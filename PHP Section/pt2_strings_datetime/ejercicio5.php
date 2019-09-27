f45768b72ac8b399e59030115507dea073cef35a
Álvaro Real Gomez <34766606+Darkenend@users.noreply.github.com>
2019-09-27 17:37:54 +0200

    Migrate ejercicio5.php to DateTime

--- PHP Section/pt2_strings_datetime/ejercicio5.php
+++ PHP Section/pt2_strings_datetime/ejercicio5.php
@@ -6,14 +6,16 @@
 </head>
 <body>
 <?php
+$start = new DateTime('first day of this month');
+$interval = new DateInterval('P1D');
+$end = new DateTime('last day of this month');
+$skip = new DatePeriod($start, $interval, $end);
 
 echo "<h1>".'&lt&lt'.'Calendario'.'>>'."</h1>";
 echo "<br><br><br><br>";
-$days_in_month = cal_days_in_month(CAL_GREGORIAN, date("n", time()), date("Y", time()));
-for ($i = 0; $i < $days_in_month; $i = $i + 1) {
-    $temp_time = ($i+1).date("n", time()).date("Y", time());
-    echo "Dia ".($i+1).": ".date("l", $temp_time)."<br>";
-}
+
+foreach ($skip as $date) echo 'Dia '.$date->format('d').': '.$date->format('l');
+
 ?>
 </body>
 </html>
