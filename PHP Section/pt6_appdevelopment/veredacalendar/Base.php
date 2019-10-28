67339e3b3becde5682048842b360098f6ff98f18
Álvaro Real Gomez <34766606+Darkenend@users.noreply.github.com>
2019-10-28 09:47:47 +0100

    Up to October 23rd's Progress (Part 4)

--- PHP Section/pt6_appdevelopment/veredacalendar/Base.php
+++ PHP Section/pt6_appdevelopment/veredacalendar/Base.php
@@ -10,13 +10,21 @@ abstract class Base {
      */
     private $endDate;
     /**
-     * @var array Multidimensional Array of vacation periods in the calendar
+     * @var array Multidimensional Array of vacation periods starts in the calendar
      */
-    private $vacationPeriods;
+    private $vacationPeriodsStart;
+    /**
+     * @var array Multidimensional Array of vacation periods ends in the calendar
+     */
+    private $vacationPeriodsEnd;
     /**
      * @var \DateTime Array of days which are festive in the calendar
      */
     private $festiveDays;
+    /**
+     * @var \DateTime Array of days when there's the evaluations
+     */
+    private $evaluationDays = [];
     /**
      * @var integer Number of days that the school year lasts
      */
@@ -26,4 +34,141 @@ abstract class Base {
      * The values represent the first, second and third periods, and the period for practices on fourth place.
      */
     private $periodLength = [1, 1, 1, 1];
+
+    function periodLengthCrafting() {
+        $xd = [];
+        $xd[0] = date_diff($this->startDate, $this->evaluationDays[0], true);
+        $xd[1] = date_diff($this->evaluationDays[0], $this->evaluationDays[1], true);
+        $xd[2] = date_diff($this->evaluationDays[1], $this->evaluationDays[2], true);
+        $xd[3] = date_diff($this->evaluationDays[2], $this->evaluationDays[3], true);
+        $this->setPeriodLength($xd);
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getStartDate(): DateTime
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param DateTime $startDate
+     */
+    public function setStartDate(DateTime $startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getEndDate(): DateTime
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param DateTime $endDate
+     */
+    public function setEndDate(DateTime $endDate): void
+    {
+        $this->endDate = $endDate;
+    }
+
+    /**
+     * @return array
+     */
+    public function getVacationPeriodsStart(): array
+    {
+        return $this->vacationPeriodsStart;
+    }
+
+    /**
+     * @param array $vacationPeriodsStart
+     */
+    public function setVacationPeriodsStart(array $vacationPeriodsStart): void
+    {
+        $this->vacationPeriodsStart = $vacationPeriodsStart;
+    }
+
+    /**
+     * @return array
+     */
+    public function getVacationPeriodsEnd(): array
+    {
+        return $this->vacationPeriodsEnd;
+    }
+
+    /**
+     * @param array $vacationPeriodsEnd
+     */
+    public function setVacationPeriodsEnd(array $vacationPeriodsEnd): void
+    {
+        $this->vacationPeriodsEnd = $vacationPeriodsEnd;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getFestiveDays(): DateTime
+    {
+        return $this->festiveDays;
+    }
+
+    /**
+     * @param DateTime $festiveDays
+     */
+    public function setFestiveDays(DateTime $festiveDays): void
+    {
+        $this->festiveDays = $festiveDays;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getEvaluationDays(): DateTime
+    {
+        return $this->evaluationDays;
+    }
+
+    /**
+     * @param DateTime $evaluationDays
+     */
+    public function setEvaluationDays(DateTime $evaluationDays): void
+    {
+        $this->evaluationDays = $evaluationDays;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCourseLength(): int
+    {
+        return $this->courseLength;
+    }
+
+    /**
+     * @param int $courseLength
+     */
+    public function setCourseLength(int $courseLength): void
+    {
+        $this->courseLength = $courseLength;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPeriodLength(): array
+    {
+        return $this->periodLength;
+    }
+
+    /**
+     * @param array $periodLength
+     */
+    public function setPeriodLength(array $periodLength): void
+    {
+        $this->periodLength = $periodLength;
+    }
 }
--- PHP Section/pt6_appdevelopment/veredacalendar/calendar.php
+++ PHP Section/pt6_appdevelopment/veredacalendar/calendar.php
@@ -1,10 +1,80 @@
 <?php
+// Variable initial declaration
+$calendarName = "";
+$startDate = null;
+$endDate = null;
+$startVacation = [];
+$endVacation = [];
+$dayVacation = [];
+$dayEvaluation = [];
+
+echo $_POST['dayEvaluation4']."<br><br><br><br><br>";
+
+// Block for variable assignation
+if (isset($_POST['calendarName'])) {
+    $calendarName = $_POST['calendarName'];
+} else {
+    $calendarName = "Default Calendar Name";
+}
+if (isset($_POST['startDate'])) {
+    $startDate = new DateTime($_POST['startDate']);
+} else {
+    $startDate = new DateTime('2019-09-09');
+}
+if (isset($_POST['endDate'])) {
+    $endDate = new DateTime($_POST['endDate']);
+} else {
+    $endDate = new DateTime('2020-06-16');
+}
+// Loops to store in Arrays
+for ($i = 0; $i < 3; $i++) {
+    $string = 'Vacation'.($i+1);
+    if (isset($_POST['start'.$string]) && $_POST['start'.$string] != "") {
+        $startVacation[$i] = new DateTime($_POST['start'.$string]);
+    }
+    if (isset($_POST['end'.$string]) && $_POST['end'.$string] != "") {
+        $endVacation[$i] = new DateTime($_POST['end'.$string]);
+    }
+}
+for ($i = 0; $i < 12; $i++) {
+    $str = 'dayVacation'.($i+1);
+    if (isset($_POST[$str]) && $_POST[$str] != "") {
+        $dayVacation[$i] = new DateTime($_POST[$str]);
+    }
+}
+for ($i = 0; $i < 4; $i++) {
+    $str = 'dayEvaluation'.($i+1);
+    if (isset($_POST[$str]) && $_POST[$str] != "") {
+        $dayEvaluation[$i] = new DateTime($_POST[$str]);
+    }
+}
+
+
+// Start of the web page, setting a Bootstrap-based layout
 require "views/html_top.views.php";
 echo "<div class=\"container-fluid pt-3\">";
 echo "<div class=\"row\">";
 echo "<div class=\"col-6 offset-3\">";
-for ($i = 0; $i < 10; $i++) {
-    echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+echo "<h1>".$calendarName."</h1><br>";
+echo "<h2>Start</h2><br>";
+echo $startDate->format('Y-m-d')."<br>";
+echo "<h2>End</h2><br>";
+echo $endDate->format('Y-m-d')."<br>";
+echo "<h3>Vacation Starts</h3><br>";
+for ($i = 0; $i < sizeof($startVacation); $i++) {
+    echo $startVacation[$i]->format('Y-m-d')."<br>";
+}
+echo "<h3>Vacation Ends</h3><br>";
+for ($i = 0; $i < sizeof($endVacation); $i++) {
+    echo $endVacation[$i]->format('Y-m-d')."<br>";
+}
+echo "<h3>Vacation Days</h3><br>";
+for ($i = 0; $i < sizeof($dayVacation); $i++) {
+    echo $dayVacation[$i]->format('Y-m-d')."<br>";
+}
+echo "<h3>Evaluation Days</h3><br>";
+for ($i = 0; $i < sizeof($dayEvaluation); $i++) {
+    echo $dayEvaluation[$i]->format('Y-m-d')."<br>";
 }
 for ($i = 0; $i < 3; $i++) {
     echo "</div>";
