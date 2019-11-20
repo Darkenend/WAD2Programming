0a4973601369437ff0eea6433b4f1da1bf48ef4c
Álvaro Real Gomez <34766606+Darkenend@users.noreply.github.com>
2019-11-20 20:08:39 +0100

    VS Code Migration

---
+++ PHP Section/pt12_framework/ex1/web/index.php
@@ -0,0 +1,9 @@
+<?php
+include(__DIR__."/../app/autoload.php");
+use Jazzyweb\Framework\Kernel;
+use Jazzyweb\Framework\Request;
+$config = Kernel::loadConfig(__DIR__."/../app/config.php");
+$kernel = new Kernel($config);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
