1abef7546ccabcf5ad847d36ede96754963e46e3
Álvaro Real Gomez <34766606+Darkenend@users.noreply.github.com>
2020-02-02 19:44:24 +0100

    February 2nd Progress

--- JavaScript Section/pt11_dom/mecanografia/js/main.js
+++ JavaScript Section/pt11_dom/mecanografia/js/main.js
@@ -1,10 +1,54 @@
-var intervalid = 0;
+/**
+ * Timeout ID that we'll use to work with timeouts.
+ */
+var timeoutid = 0;
+/**
+ * Timer interval ID that we'll use to work with a timer.
+ */
+var timerid = 0;
+/**
+ * Amount of seconds that have passed.
+ */
+var seconds = 0;
+
+
+// * Document elements to use quickly
+/**
+ * Main Panel where the mode can be selected
+ */
 var mainpanel = document.getElementById('main-panel');
+/**
+ * Game Panel where the keys to press appear
+ */
 var gamepanel = document.getElementById('game-panel');
+/**
+ * Panel where the info is given for the user to check their stats.
+ */
 var inforow = document.getElementById('info-row');
-var characterarray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+/**
+ * Default character array
+ */
+var characterarray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+/**
+ * The array of keys to currently press.
+ */
 var currentarray = [];
+
+/**
+ * Is the round currently active?
+ */
+var roundactive = false;
+
+/**
+ * Successful strokes in the round
+ */
+var goodstrokes = 0;
+/**
+ * Errors done in the round
+ */
+var badstrokes = 0;
+
 /**
  * Integer that determines the mode currently applied
  * * 1: PPM (Keystrokes Per Minute)
@@ -14,7 +58,6 @@ var currentarray = [];
  *  * From 27 strokes onwards: 2s timer
  *  * From 54 strokes onwards: 1s timer
  *  * From 108 strokes onwards: 0.5s timer
- * 
  */
 var mode = 0;
 
@@ -26,29 +69,72 @@ function keyHandling(event) {
     //* IE Compatibility
     var myEvent = event || window.event;
     var code = myEvent.code;
+    if (roundactive) {
+        var pupper = 0;
+        var hit = -1;
+        console.log("Round is active, processing keypress");
+        // We check all to see if there's a success
+        for (var i = 0; i < currentarray.length; i++) {
+            if (code == 'Key'+currentarray[i]) {
+                console.log("Match found");
+                pupper++;
+                hit = i;
+            }
+        }
+        if (pupper === 0) {
+            console.log("Bad Hit");
+            badstrokes++;
+        } else {
+            console.log("Good Hit");
+            goodstrokes++;
+            currentarray.splice(hit, 1);
+            addCharacter();
+            updateGameScreen();
+            mode3Interval();
+        }
+    } else {
+        console.log("Round isn't active currently.")
+    }
+}
 
+function updateGameScreen() {
+    gamepanel.innerHTML = "";
+    for (var i = 0; i < currentarray.length; i++) {
+        var element = document.createElement('h1');
+        element.innerHTML = currentarray[i];
+        gamepanel.appendChild(element);
+    }
+}
+
+function mode3Interval() {
+    if (mode === 3) {
+        clearInterval(intervalid);
+        if (goodstrokes > 26) {
+            if (goodstrokes > 53) {
+                if (goodstrokes > 107) intervalid = setTimeout(error, 500);
+                else intervalid = setTimeout(error, 1000);
+            } else intervalid = setTimeout(error, 2000);
+        } else intervalid = setTimeout(error, 60000);
+    }
 }
 
 /**
  * This function activates the keystrokes per minute mode
- * 
- * TODO: Implement KPM Mode Activation
  */
 function activateKPM() {
+    document.getElementById('current-mode').innerHTML = "Modo: Pulsaciones por Minuto";
     console.log("'KPM' mode");
     mode = 1;
-    toggleInfobarButtons();
-    //* Uncomment the next line when it's modified to have the challenge buttons activated by default
-    // if (document.getElementById('challenge-btn').disabled) toggleChallengeButtons();
+    if (document.getElementById('start-btn').disabled) toggleInfobarButtons();
+    if (document.getElementById('challenge-btn').disabled) toggleChallengeButtons();
 }
 
 /**
  * This function activates the challenge modes options to choose from
- * 
- * TODO: Implement Challenge Mode Activation
  */
 function activateChallenge() {
     console.log("'Challenge' mode");
+    toggleInfobarButtons();
     toggleChallengeButtons();
 }
 
@@ -56,49 +142,103 @@ function activateChallenge() {
  * This function activates the sudden death mode
  */
 function activateSuddenDeath() {
+    document.getElementById('current-mode').innerHTML = "Modo: Muerte Súbita";
     console.log("'Sudden Death' mode");
     mode = 2;
-    toggleInfobarButtons();
+    if (document.getElementById('start-btn').disabled) toggleInfobarButtons();
 }
 
 /**
  * This function activates the timed mode
  */
 function activateTimed() {
+    document.getElementById('current-mode').innerHTML = "Modo: Contrarreloj";
     console.log("'Timed' mode");
     mode = 3;
-    toggleInfobarButtons();
+    if (document.getElementById('start-btn').disabled) toggleInfobarButtons();
 }
 
 /**
  * This function starts the round
- * 
- * TODO: Implement Start Activation
  */
 function start() {
-    console.log("Start");
-    toggleChoiceButtons();
+    goodstrokes = 0;
+    badstrokes = 0;
+    seconds = 0;
+    console.log("Variables reset, Starting now");
+    mainpanel.hidden = true;
+    switch (mode) {
+        case 1:
+            console.log("Starting KPM mode");
+            intervalid = setTimeout(end, 60000);
+            game();
+            break;
+        case 2:
+            console.log("Starting sudden death");
+            game();
+            break;
+        case 3:
+            console.log("Starting mode Timed Challenge");
+            intervalid = setTimeout(error, 60000);
+            game();
+            break;
+        default:
+            console.log('mode :', mode);
+            break;
+    }
+}
+
+/**
+ * This function properly sets the round and the checks
+ */
+function game() {
+    for (var i = 0; i < 5; i++) addCharacter();
+    console.log('currentarray :', currentarray);
+    updateGameScreen();
+    roundactive = true;
+    timerid = setInterval(timer, 1000);
 }
 
 /**
  * This function stops the round
- * 
- * TODO: Implement Round Interruption
  */
 function stop() {
-    console.log("Stop");
-    end();
+    if (roundactive) {
+        console.log("Stop");
+        end();
+    }
+}
+
+/**
+ * This function is called on timed challenged and there's been too long since the last keystroke
+ */
+function error() {
+    badstrokes++;
+    if (mode === 3) {
+        clearInterval(intervalid);
+        if (goodstrokes > 26) {
+            if (goodstrokes > 53) {
+                if (goodstrokes > 107) intervalid = setTimeout(error, 500);
+                else intervalid = setTimeout(error, 1000);
+            } else intervalid = setTimeout(error, 2000);
+        } else intervalid = setTimeout(error, 60000);
+    }
 }
 
 /**
  * This function ends the round
- * 
- * TODO: Implement End of Round
  */
 function end() {
     console.log("End");
-    mode = 0;
+    if (mode === 3 || mode === 1) clearTimeout(intervalid);
+    clearInterval(timerid);
+    updateStats()
     toggleInfobarButtons();
+    roundactive = false;
+    mode = 0;
+    gamepanel.innerHTML = "";
+    mainpanel.hidden = false;
+    
 }
 
 /**
@@ -112,7 +252,7 @@ function mainpanelAppearToggle() {
  * This function toggles all the buttons for choices in the main panel
  */
 function toggleChoiceButtons() {
-    toggleChoiceButtons();
+    toggleInfobarButtons();
     toggleChallengeButtons();
 }
 
@@ -146,5 +286,23 @@ function toggleChallengeButtons() {
  * This function adds a new character to the current array that we're gonna work on.
  */
 function addCharacter() {
-    currentarray.push(characterarray[Math.random() * (characterarray.length)]);
+    var temp = Math.floor(Math.random() * (characterarray.length));
+    var tempCharacter = characterarray[temp];
+    currentarray.push(tempCharacter);
+}
+
+/**
+ * This function is called every second so we can display in real time the seconds done.
+ */
+function timer() {
+    seconds++;
+    updateStats();
+    document.getElementById('time-remaining').innerHTML = "Tiempo: "+seconds;
+    if (mode === 1 && seconds === 60 || mode === 2 && badstrokes != 0 || mode === 3 && badstrokes === 5) end();
+}
+
+function updateStats() {
+    var kpm = (60*(goodstrokes+badstrokes)/seconds);
+    document.getElementById('result-field').innerHTML = "Puntuación: "+(goodstrokes-badstrokes*2);
+    document.getElementById('kpm').innerHTML = "PPM: "+kpm.toFixed(2);
 }
